25c542e4e1e2567ac7a46d3584bde3179a2f13e6
Kasun Perera <kasper52786@gmail.com>
2021-01-22 22:24:49 +0530

    Add swagger documentation

--- src/main/java/ca/randoli/assignment/controller/EventController.java
+++ src/main/java/ca/randoli/assignment/controller/EventController.java
@@ -1,5 +1,7 @@
 package ca.randoli.assignment.controller;
 
+import ca.randoli.assignment.dto.EventDTO;
+import ca.randoli.assignment.model.Event;
 import ca.randoli.assignment.response.GenericResponse;
 import ca.randoli.assignment.service.EventService;
 import ca.randoli.assignment.service.RecordService;
@@ -8,11 +10,15 @@ import org.apache.camel.Exchange;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.apache.camel.model.rest.RestParamType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 
 @Component
@@ -38,10 +44,14 @@ public class EventController extends RouteBuilder {
     @Override
     public void configure() throws Exception {
         restConfiguration()
-                .contextPath(environment.getProperty("camel.component.servlet.mapping.contextPath", "/api/*"))
                 .component("servlet")
+                .enableCORS(true)
+                .contextPath("/api")
+                .apiContextPath("/swagger")
+                .apiContextRouteId("swagger")
                 .apiProperty("api.title", "Randoli Events API")
                 .apiProperty("api.version", "1.0")
+                .apiProperty("base.path", "api")
                 .apiProperty("cors", "true")
                 .port(environment.getProperty("server.port", "8080"))
                 .bindingMode(RestBindingMode.json);
@@ -51,19 +61,47 @@ public class EventController extends RouteBuilder {
                 .consumes(MediaType.APPLICATION_JSON_VALUE)
 
                 // GET /api/events
-                .get("/").description("Get all events")
+                .get("/")
+                    .description("Get all events")
+                    .outType(Event[].class)
+                    .responseMessage()
+                        .code(200)
+                    .endResponseMessage()
                 .route()
                 .bean(EventService.class, "findAll")
                 .endRest()
 
                 // GET /api/events/{eventId}
-                .get("/{eventId}").description("Get event by id")
+                .get("/{eventId}")
+                    .description("Get event by id")
+                    .param()
+                        .name("eventId")
+                        .type(RestParamType.path)
+                        .required(true)
+                        .description("UUID of the event")
+                    .endParam()
+                    .responseMessage()
+                        .code(200)
+                        .responseModel(Event.class)
+                    .endResponseMessage()
                 .route()
                 .bean(EventService.class, "findEventById(${header.eventId})")
                 .endRest()
 
                 // POST /api/events
-                .post("/").description("Create new event")
+                .post("/")
+                    .description("Create new event")
+                    .type(EventDTO.class)
+                    .param()
+                        .name("body")
+                        .type(RestParamType.body)
+                        .required(true)
+                        .description("The event to create")
+                    .endParam()
+                    .responseMessage()
+                        .code(201)
+                        .responseModel(Event.class)
+                    .endResponseMessage()
                 .route()
                 .marshal().json()
                 .unmarshal(eventDtoDataFormat)
@@ -72,14 +110,37 @@ public class EventController extends RouteBuilder {
                 .endRest()
 
                 // DELETE /api/events/{eventId}
-                .delete("/{eventId}").description("Delete event by id")
+                .delete("/{eventId}")
+                    .description("Delete event by id")
+                    .param()
+                        .name("eventId")
+                        .type(RestParamType.path)
+                        .required(true)
+                        .description("UUID of the event")
+                    .endParam()
+                    .responseMessage()
+                        .code(204)
+                        .responseModel(Event.class)
+                    .endResponseMessage()
                 .route()
                 .bean(EventService.class, "removeEvent(${header.eventId})")
                 .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(204))
                 .endRest()
 
                 // PATCH /api/events/{eventId}
-                .patch("/{eventId}").description("Update event by id")
+                .patch("/{eventId}")
+                    .description("Update event by id")
+                    .type(EventDTO.class)
+                    .param()
+                        .name("eventId")
+                        .type(RestParamType.path)
+                        .required(true)
+                        .description("UUID of the event")
+                    .endParam()
+                    .responseMessage()
+                        .code(201)
+                        .responseModel(Event.class)
+                    .endResponseMessage()
                 .route()
                 .marshal().json()
                 .unmarshal(eventDtoDataFormat)
@@ -92,7 +153,8 @@ public class EventController extends RouteBuilder {
                 .consumes(MediaType.APPLICATION_JSON_VALUE)
 
                 // POST /api/records
-                .post("/").description("Create events from records")
+                .post("/")
+                    .description("Create events from records")
                 .route()
                 .to("direct:processEvents")
                 .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(202))
